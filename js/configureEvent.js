71597a2013a36adccdc0c2ff2e0083a9a1b595ee
zhufeng <531232196@qq.com>
2018-03-09 09:51:22 +0800

    all

--- js/configureEvent.js
+++ js/configureEvent.js
@@ -1,62 +1,62 @@
-$(function(){
+$(function() {
   $("#group-type").hide();
   $("#label-group").hide();
   $("#random-group").hide();
 
   $("#tab_hide").hide();
   $("#tab_content").css({
-    marginTop:0
+    marginTop: 0
   })
 
   $("#tab_hide_random").hide();
 
-  $("input:radio[name='group']").change(function (e){
-      var val = $("input:radio[name='group']:checked").val();
+  $("input:radio[name='group']").change(function(e) {
+    var val = $("input:radio[name='group']:checked").val();
 
-      switch (val) {
-        case "0":
-          $("#group-type").hide();
-          $("#tab_hide").hide();
-          $("#tab_content").css({
-            marginTop:0
-          })
+    switch (val) {
+      case "0":
+        $("#group-type").hide();
+        $("#tab_hide").hide();
+        $("#tab_content").css({
+          marginTop: 0
+        })
 
-          $(".tab_hide_random").hide();
+        $(".tab_hide_random").hide();
         break;
 
-        case "1":
-          $("#group-type").show();
-          $("#label-group").show();
-          $("#random-group").hide();
+      case "1":
+        $("#group-type").show();
+        $("#label-group").show();
+        $("#random-group").hide();
 
-          $("#tab_hide").show();
-          $("#tab_content").css({
-            marginTop:'-30px'
-          })
+        $("#tab_hide").show();
+        $("#tab_content").css({
+          marginTop: '-30px'
+        })
 
-          $(".tab_hide_random").hide();
+        $(".tab_hide_random").hide();
         break;
 
-        case "2":
-          $("#group-type").show();
-          $("#label-group").hide();
-          $("#random-group").show();
+      case "2":
+        $("#group-type").show();
+        $("#label-group").hide();
+        $("#random-group").show();
 
-          $("#tab_hide").hide();
+        $("#tab_hide").hide();
 
-          $("#random_group_select").val("1")
-          $("#tab_hide_random").show();
-          $(".tab_hide_random").eq(0).show();
-          $(".tab_hide_random").slice(1,5).hide();
-          $(".group_box").css({
-            marginTop:'-30px'
-          })
+        $("#random_group_select").val("1")
+        $("#tab_hide_random").show();
+        $(".tab_hide_random").eq(0).show();
+        $(".tab_hide_random").slice(1, 5).hide();
+        $(".group_box").css({
+          marginTop: '-30px'
+        })
         break;
 
-        default:
+      default:
 
         break;
-      }
+    }
   });
 
 
@@ -64,53 +64,53 @@ $(function(){
 
   $("#send_date").hide();
 
-  $("input:radio[name='group_frequency']").change(function(e){
-      var val = $("input:radio[name='group_frequency']:checked").val();
+  $("input:radio[name='group_frequency']").change(function(e) {
+    var val = $("input:radio[name='group_frequency']:checked").val();
 
-      switch (val) {
-        case "0":
-          $("#frequency_weektime").hide();
-          $("#send_date").hide();
-          break;
+    switch (val) {
+      case "0":
+        $("#frequency_weektime").hide();
+        $("#send_date").hide();
+        break;
 
-        case "1":
-          $("#frequency_weektime").hide();
-          $("#send_date").show();
-          break;
+      case "1":
+        $("#frequency_weektime").hide();
+        $("#send_date").show();
+        break;
 
-        case "2":
-          $("#frequency_weektime").show();
-          $("#frequency_week").hide();
-          $("#frequency_time").show();
-          $("#send_date").hide();
-          break;
+      case "2":
+        $("#frequency_weektime").show();
+        $("#frequency_week").hide();
+        $("#frequency_time").show();
+        $("#send_date").hide();
+        break;
 
-        case "3":
-          $("#frequency_weektime").show();
-          $("#frequency_week").show();
-          $("#frequency_time").show();
-          $("#send_date").hide();
-          break;
+      case "3":
+        $("#frequency_weektime").show();
+        $("#frequency_week").show();
+        $("#frequency_time").show();
+        $("#send_date").hide();
+        break;
 
-        default:
+      default:
 
-      }
+    }
   })
 
 
-  $('#random_group_select').change(function(){
+  $('#random_group_select').change(function() {
 
     var val = $(this).children('option:selected').val();
     $(".tab_hide_random").show();
-    $(".tab_hide_random").slice(val,5).hide();
+    $(".tab_hide_random").slice(val, 5).hide();
   })
 
 
   $(".form_datetime").datetimepicker({
     minView: "month", //选择日期后，不会再跳转去选择时分秒
-    language:  'zh-CN',
+    language: 'zh-CN',
     format: 'yyyy-mm-dd',
-    todayBtn:  1,
+    todayBtn: 1,
     autoclose: 1,
   });
 
@@ -119,18 +119,23 @@ $(function(){
   $("#fq_hide").hide();
 
 
-  $("#btn_active_ok").click(function(){
+  $("#btn_active_ok").click(function() {
     $("#activity_modal").modal('hide');
     $("#ygw_hide").show();
   })
 
-  $("#btn_active2_ok").click(function(){
+  $("#btn_active2_ok").click(function() {
     $("#activity_modal2").modal('hide');
     $("#fq_hide").show();
   })
 
-  $("#btn_preview").click(function(){
+  $("#btn_make_preview").click(function() {
     var content = $("#txt_content").text();
     $("#txt_content_right").text(content)
+
+    $('.preview_header').css({
+      'background-color': '#6666FF',
+      'color': '#fff'
+    });
   })
 })
